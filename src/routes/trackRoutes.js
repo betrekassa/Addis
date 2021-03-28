50532908431a122eb150da5717d72e68d2ebb85c
betrekassa <kassa_ethiopia@yahoo.com>
2021-03-28 14:07:44 -0700

    Get all

---
+++ src/routes/trackRoutes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireAuth = require('../middleware/requireAuth');
+
+const Track = mongoose.model('Track');
+
+const router = express.Router();
+const cors = require('cors');
+
+router.use(cors());
+router.use(requireAuth);
+
+router.get('/tracks',async(req,res)=>{
+
+    //console.log(req.user._id);
+    const tracks = await Track.find({userId: req.user._id});
+    res.send(tracks);
+});
+
+router.post('/tracks',async(req,res) =>
+    {
+        const {name, location} = req.body;
+       
+        if (!name || !location) 
+            {
+                return res.status(422).send({error: 'You must provide name & Location'});
+            }
+        try
+        {
+            const track = new Track({name, location, userId: req.user._id});
+            await track.save();
+            res.send(track);
+        } catch(err)
+        {
+            return res.status(422).send({error: 'You must provide name & Location'});
+        }
+    });
+
+    module.exports = router;
